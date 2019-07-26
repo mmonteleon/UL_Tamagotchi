2a8a71bdbfa06baa9cb17bbb03338f26681d2dba
EC2 Default User <ec2-user@ip-172-31-35-127.us-east-2.compute.internal>
2019-07-26 16:37:26 +0000

    creating lab version

--- Tamogotchi.py
+++ Tamogotchi.py
@@ -1,23 +1,24 @@
-# Check out Old Skool Tamagotchi: https://www.youtube.com/watch?v=U9at2BCLFhY and the new ones coming out: https://www.bandai.com/tamagotchi/
+# Check out Old Skool Tamagotchi: https://www.youtube.com/watch?v=U9at2BCLFhY 
+# and the new ones coming out: https://www.bandai.com/tamagotchi/
 
+###############################################################################
 
-class Tamagotchi:
-  def __init__(self, the_name):
-    self.name = the_name
-    self.health = 5
-    self.happiness = 5
-    self.potty_trained = False
-    self.food = 5
-    self.exercise = 5
-    print("Tamagotchi created!")
+# Code your class here #
+
+###############################################################################
     
-    #1.
+    #1
+    # Create a Tamagotchi class.
+    # It should have the following attributes: name, health_level, happiness_level,
+    # isPottyTrained (this is boolean), food_level.  At the end the __init__ method
+    # you create, have it print "Tamagotchi created!"
+    
+    
+    #2.
     # Write a __repr__ method that allows us to print a a Tamagotchi
     
     
-    
-        
-    # 2. 
+    # 3. 
     # Define a method that determines if the Tamagotchi is hungry.
     # A Tamagotchi is full if its food level is 5.
     # A Tamagotchi is satisfied if its food level is 3 or 4
@@ -25,12 +26,11 @@ class Tamagotchi:
     # A Tamagotchi is starving if its foold level is zero
     
     
-    
-    
-    # 3.
+    # 4.
     # Define a method that "feeds" the Tamagotchi
     # Increase the food level of the Tamagotchi
-    # Print a statement something like this "<Tanagotchi name> ate <food>.  New hunger level: <hunger level>" (Format it however you would like.)
+    # Print a statement something like this "<Tanagotchi name> ate <food>.  
+    # New hunger level: <hunger level>" (Format it however you would like.)
     
     
     
--- TamogotchiTester.py
+++ TamogotchiTester.py
@@ -1,2 +1,6 @@
-from Tamogotchi import Tamagotchi
+import Tamagotchi
 # Since the Tamagotchi class is inside the Tamagotchi.py file, we have to import it in order to be able to use it.
+
+# Create your Tamogotchi game here!
+# Play with your Tamagotchi pet and "play" with it using the methods you wrote
+# in your Tamogotchi class.
